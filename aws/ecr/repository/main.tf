6200843bb1b0db6bc23b393cc70fd1cd6741ea6e
SebRamsland <mail@sebastianramsland.com>
2022-02-15 15:57:00 +0100

    Remove unnecessary variable

--- aws/ecr/repository/main.tf
+++ aws/ecr/repository/main.tf
@@ -32,7 +32,7 @@ data "aws_iam_policy_document" "allow_pull_image_cross_account" {
 }
 
 resource "aws_iam_policy" "push_image" {
-  name        = var.push_image_iam_policy_name
+  name        = replace("${aws_ecr_repository.this.name}-ecr-repository-push-image", "/", "-")
   description = "Provides access to push images to ${aws_ecr_repository.this.name} container repository."
   policy      = data.aws_iam_policy_document.allow_push_image.json
   tags        = var.tags
--- aws/ecr/repository/variables.tf
+++ aws/ecr/repository/variables.tf
@@ -14,10 +14,6 @@ variable "allow_pull_from_aws_account_ids" {
   type = list(string)
 }
 
-variable "push_image_iam_policy_name" {
-  type = string
-}
-
 variable "tags" {
   type = map(string)
 }
