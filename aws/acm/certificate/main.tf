b2fb2ef42217ae371692ca0bb685dce4cf048b79
SebRamsland <mail@sebastianramsland.com>
2022-02-16 15:33:01 +0100

    Simplify certificate validation

--- aws/acm/certificate/main.tf
+++ aws/acm/certificate/main.tf
@@ -16,22 +16,18 @@ resource "aws_acm_certificate_validation" "this" {
 }
 
 resource "aws_route53_record" "certificate_validation" {
-
-  depends_on = [aws_acm_certificate.this]
-
-  for_each = var.validation_method == "DNS" ? {
+  for_each = {
     for dvo in aws_acm_certificate.this.domain_validation_options : dvo.domain_name => {
-      name    = dvo.resource_record_name
-      record  = dvo.resource_record_value
-      type    = dvo.resource_record_type
-      zone_id = var.zone_id
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
     }
-  } : {}
+  }
 
   allow_overwrite = true
   name            = each.value.name
   records         = [each.value.record]
   ttl             = 60
   type            = each.value.type
-  zone_id         = each.value.zone_id
+  zone_id         = var.zone_id
 }
