7a3a0d4e50ce3d062b2732e0eb5891d97dae5c9b
SebRamsland <mail@sebastianramsland.com>
2022-02-19 14:51:36 +0100

    Add iam role policy module

---
+++ aws/iam/role-policies/cloudwatch-write-logs/main.tf
@@ -0,0 +1,16 @@
+resource "aws_iam_role_policy" "this" {
+  role   = var.role_id
+  name   = "CloudWatchWriteLogs"
+  policy = data.aws_iam_policy_document.cloudwatch_write_logs.json
+}
+
+data "aws_iam_policy_document" "cloudwatch_write_logs" {
+  statement {
+    effect    = "Allow"
+    resources = var.log_group_arns
+    actions   = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+  }
+}
---
+++ aws/iam/role-policies/cloudwatch-write-logs/variables.tf
@@ -0,0 +1,7 @@
+variable "role_id" {
+  type = string
+}
+
+variable "log_group_arns" {
+  type = list(string)
+}
