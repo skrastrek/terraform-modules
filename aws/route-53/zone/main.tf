3ebd0900e083086026d8174953edd7016e366aea
Sebastian Ramsland <sebastian.ramsland@dnb.no>
2024-01-17 18:42:01 +0100

    Module improvements

--- aws/route-53/zone/main.tf
+++ aws/route-53/zone/main.tf
@@ -1,13 +1,26 @@
+data "aws_caller_identity" "current" {}
+
 resource "aws_route53_zone" "this" {
   name    = var.domain_name
   comment = var.comment
+
+  dynamic "vpc" {
+    for_each = var.vpc_associations
+    content {
+      vpc_id     = vpc.value.vpc_id
+      vpc_region = vpc.value.vpc_region
+    }
+  }
+
+  tags = var.tags
+
+  lifecycle {
+    ignore_changes = [vpc]
+  }
 }
 
 resource "aws_route53_record" "this" {
-
-  for_each = {
-    for record in var.records : "${record.name}_${record.type}" => record
-  }
+  for_each = var.records
 
   zone_id = aws_route53_zone.this.id
   name    = each.value.name
@@ -17,13 +30,15 @@ resource "aws_route53_record" "this" {
 }
 
 resource "aws_route53_hosted_zone_dnssec" "this" {
-  count          = var.dnssec_enabled ? 1 : 0
+  count = var.dnssec_enabled ? 1 : 0
+
   hosted_zone_id = aws_route53_zone.this.zone_id
   depends_on     = [aws_route53_key_signing_key.dnssec[0]]
 }
 
 resource "aws_route53_key_signing_key" "dnssec" {
-  count                      = var.dnssec_enabled ? 1 : 0
+  count = var.dnssec_enabled ? 1 : 0
+
   name                       = var.domain_name
   hosted_zone_id             = aws_route53_zone.this.zone_id
   key_management_service_arn = aws_kms_key.dnssec[0].arn
@@ -38,67 +53,60 @@ resource "aws_kms_key" "dnssec" {
   deletion_window_in_days = 7
 
   policy = data.aws_iam_policy_document.kms_key_dnssec.json
+
+  tags = var.tags
 }
 
 data "aws_iam_policy_document" "kms_key_dnssec" {
   statement {
+    sid    = "IAM User Permissions"
+    effect = "Allow"
+
+    actions = ["kms:*"]
+
+    resources = ["*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+  }
+
+  statement {
     sid    = "Allow Route 53 DNSSEC Service"
     effect = "Allow"
+
     actions = [
       "kms:DescribeKey",
       "kms:GetPublicKey",
       "kms:Sign",
     ]
 
-    principals {
-      type = "Service"
-      identifiers = [
-        "dnssec-route53.amazonaws.com"
-      ]
-    }
+    resources = ["*"]
 
-    resources = [
-      "*"
-    ]
+    principals {
+      type        = "Service"
+      identifiers = ["dnssec-route53.amazonaws.com"]
+    }
   }
 
   statement {
     sid    = "Allow Route 53 DNSSEC Service to CreateGrant"
     effect = "Allow"
-    actions = [
-      "kms:CreateGrant"
-    ]
+
+    actions = ["kms:CreateGrant"]
+
+    resources = ["*"]
 
     principals {
-      type = "Service"
-      identifiers = [
-        "dnssec-route53.amazonaws.com"
-      ]
+      type        = "Service"
+      identifiers = ["dnssec-route53.amazonaws.com"]
     }
-
-    resources = [
-      "*"
-    ]
 
     condition {
       test     = "Bool"
       variable = "kms:GrantIsForAWSResource"
       values   = ["true"]
     }
-  }
-
-  statement {
-    sid    = "IAM User Permissions"
-    effect = "Allow"
-    actions = [
-      "kms:*"
-    ]
-
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
-    }
-
-    resources = ["*"]
   }
 }
--- aws/route-53/zone/variables.tf
+++ aws/route-53/zone/variables.tf
@@ -7,17 +7,29 @@ variable "comment" {
   default = ""
 }
 
-variable "records" {
+variable "vpc_associations" {
   type = list(object({
+    vpc_id     = string
+    vpc_region = string
+  }))
+  default = []
+}
+
+variable "records" {
+  type = map(object({
     type    = string
     name    = string
     records = list(string)
     ttl     = number
   }))
-  default = []
+  default = {}
 }
 
 variable "dnssec_enabled" {
   type    = bool
   default = false
+}
+
+variable "tags" {
+  type = map(string)
 }
