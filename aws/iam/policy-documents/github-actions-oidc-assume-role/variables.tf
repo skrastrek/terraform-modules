4ab90fd657a4641e0a60b9ac70108dee4c17089f
SebRamsland <mail@sebastianramsland.com>
2024-04-04 10:58:20 +0200

    Add github actions oidc assume role iam policy document

---
+++ aws/iam/policy-documents/github-actions-oidc-assume-role/variables.tf
@@ -0,0 +1,10 @@
+variable "github_actions_iam_oidc_provider_arn" {
+  type = string
+}
+
+variable "github_repositories" {
+  type = list(object({
+    organization = string
+    repository   = string
+  }))
+}
