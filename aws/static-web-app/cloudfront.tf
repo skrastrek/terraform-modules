3093b276f9ad4b88783605282860f05314a23965
SebRamsland <mail@sebastianramsland.com>
2024-04-17 17:35:36 +0200

    Add policies

--- aws/static-web-app/cloudfront.tf
+++ aws/static-web-app/cloudfront.tf
@@ -11,6 +11,22 @@ data "aws_cloudfront_cache_policy" "caching_disabled" {
   id = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad"
 }
 
+data "aws_cloudfront_origin_request_policy" "cors_custom_origin" {
+  id = "59781a5b-3903-41f3-afcb-af62929ccde1"
+}
+
+data "aws_cloudfront_origin_request_policy" "all_viewer" {
+  id = "216adef6-5c7f-47e4-b989-5492eafa07d3"
+}
+
+data "aws_cloudfront_origin_request_policy" "all_viewer_except_host_header" {
+  id = "b689b0a8-53d0-40ab-baf2-68738e2966ac"
+}
+
+data "aws_cloudfront_response_headers_policy" "security_headers" {
+  id = "67f7725c-6f97-4210-82d7-5512b31e9d03"
+}
+
 resource "aws_cloudfront_origin_access_control" "this" {
   name                              = "${var.name_prefix}-s3-bucket"
   origin_access_control_origin_type = "s3"
@@ -93,13 +109,39 @@ resource "aws_cloudfront_distribution" "this" {
 
     viewer_protocol_policy = "redirect-to-https"
 
-    cache_policy_id = data.aws_cloudfront_cache_policy.caching_disabled.id
+    cache_policy_id            = data.aws_cloudfront_cache_policy.caching_disabled.id
+    origin_request_policy_id   = data.aws_cloudfront_origin_request_policy.all_viewer.id
+    response_headers_policy_id = data.aws_cloudfront_response_headers_policy.security_headers.id
 
     dynamic "function_association" {
       for_each = var.spa_enabled ? [aws_cloudfront_function.spa_redirect[0].arn] : []
       content {
         event_type   = "viewer-request"
         function_arn = function_association.value
+      }
+    }
+  }
+
+  dynamic "ordered_cache_behavior" {
+    for_each = var.auth_routes
+    content {
+      path_pattern     = ordered_cache_behavior.value.path_pattern
+      target_origin_id = local.auth_origin_id
+
+      allowed_methods = ["GET", "HEAD"]
+      cached_methods  = ["GET", "HEAD"]
+
+      compress = true
+
+      viewer_protocol_policy = "redirect-to-https"
+
+      cache_policy_id            = data.aws_cloudfront_cache_policy.caching_disabled.id
+      origin_request_policy_id   = data.aws_cloudfront_origin_request_policy.all_viewer.id
+      response_headers_policy_id = data.aws_cloudfront_response_headers_policy.security_headers.id
+
+      lambda_function_association {
+        event_type = "viewer-request"
+        lambda_arn = ordered_cache_behavior.value.function_arn
       }
     }
   }
@@ -123,45 +165,19 @@ resource "aws_cloudfront_distribution" "this" {
     }
   }
 
-  dynamic "ordered_cache_behavior" {
-    for_each = var.auth_routes
-    content {
-      path_pattern     = ordered_cache_behavior.value.path_pattern
-      target_origin_id = local.auth_origin_id
-
-      allowed_methods = ["GET", "HEAD"]
-      cached_methods  = ["GET", "HEAD"]
-
-      compress = true
-
-      viewer_protocol_policy = "redirect-to-https"
-
-      forwarded_values {
-        query_string = true
-
-        cookies {
-          forward = "none"
-        }
-      }
-
-      lambda_function_association {
-        event_type = "viewer-request"
-        lambda_arn = ordered_cache_behavior.value.function_arn
-      }
-    }
-  }
-
   default_cache_behavior {
     target_origin_id = local.s3_bucket_origin_id
-
-    viewer_protocol_policy = "redirect-to-https"
-
-    cache_policy_id = data.aws_cloudfront_cache_policy.caching_optimized.id
 
     allowed_methods = ["GET", "HEAD"]
     cached_methods  = ["GET", "HEAD"]
 
     compress = true
+
+    viewer_protocol_policy = "redirect-to-https"
+
+    cache_policy_id            = data.aws_cloudfront_cache_policy.caching_optimized.id
+    origin_request_policy_id   = data.aws_cloudfront_origin_request_policy.all_viewer.id
+    response_headers_policy_id = data.aws_cloudfront_response_headers_policy.security_headers.id
 
     dynamic "function_association" {
       for_each = var.spa_enabled ? [aws_cloudfront_function.spa_redirect[0].arn] : []
