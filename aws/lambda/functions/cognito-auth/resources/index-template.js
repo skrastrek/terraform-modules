ffbe330b912144daa60bf335a85764937606d9f4
SebRamsland <mail@sebastianramsland.com>
2024-04-17 16:19:33 +0200

    Remove comments

--- aws/lambda/functions/cognito-auth/resources/index-template.js
+++ aws/lambda/functions/cognito-auth/resources/index-template.js
@@ -1,11 +1,10 @@
 const {Authenticator} = require('cognito-at-edge');
 
 const authenticator = new Authenticator({
-    // Replace these parameter values with those of your own environment
-    region: `${cognito_user_pool_region_id}`, // user pool region
-    userPoolId: `${cognito_user_pool_id}`, // user pool ID
-    userPoolAppId: `${cognito_user_pool_client_id}`, // user pool app client ID
-    userPoolDomain: `${cognito_user_pool_domain}`, // user pool domain
+    region: `${cognito_user_pool_region_id}`,
+    userPoolId: `${cognito_user_pool_id}`,
+    userPoolAppId: `${cognito_user_pool_client_id}`,
+    userPoolDomain: `${cognito_user_pool_domain}`,
 });
 
 exports.handler = async (request) => authenticator.handle(request);
