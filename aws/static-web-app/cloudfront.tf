8ea8a3cd7eb872ea11e53b7951563f8c308ac80d
SebRamsland <mail@sebastianramsland.com>
2024-04-17 15:36:02 +0200

    Conditionally set function association

--- aws/static-web-app/cloudfront.tf
+++ aws/static-web-app/cloudfront.tf
@@ -93,10 +93,10 @@ resource "aws_cloudfront_distribution" "this" {
     cache_policy_id = data.aws_cloudfront_cache_policy.caching_disabled.id
 
     dynamic "lambda_function_association" {
-      for_each = var.spa_enabled ? aws_cloudfront_function.spa_redirect : []
+      for_each = var.spa_enabled ? aws_cloudfront_function.spa_redirect[0].arn : []
       content {
         event_type = "viewer-request"
-        lambda_arn = function_association.value.arn
+        lambda_arn = lambda_function_association.value
       }
     }
   }
@@ -154,7 +154,7 @@ resource "aws_cloudfront_distribution" "this" {
     compress = true
 
     dynamic "lambda_function_association" {
-      for_each = var.spa_enabled ? [aws_cloudfront_function.spa_redirect.arn] : []
+      for_each = var.spa_enabled ? [aws_cloudfront_function.spa_redirect[0].arn] : []
       content {
         event_type = "viewer-request"
         lambda_arn = lambda_function_association.value
