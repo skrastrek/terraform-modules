281df060ad54017a43be273fa82b37b1668124f8
SebRamsland <mail@sebastianramsland.com>
2024-07-19 09:42:05 +0200

    Change variable type to object for reusability

--- aws/lambda/jwt-authorizer/main.tf
+++ aws/lambda/jwt-authorizer/main.tf
@@ -46,14 +46,14 @@ resource "aws_lambda_function" "this" {
 
   environment {
     variables = {
-      JWT_AUDIENCE            = var.jwt_audience != null ? join(",", var.jwt_audience) : null
-      JWT_COGNITO_CLIENT_ID   = var.jwt_cognito_client_id != null ? join(",", var.jwt_cognito_client_id) : null
-      JWT_COGNITO_GROUP       = var.jwt_cognito_group != null ? join(",", var.jwt_cognito_group) : null
-      JWT_COGNITO_TOKEN_USE   = var.jwt_cognito_token_use
-      JWT_ISSUER              = var.jwt_issuer
-      JWT_SCOPE               = var.jwt_scope != null ? join(",", var.jwt_scope) : null
-      JWT_SOURCE_HEADER_NAME  = var.jwt_source_header_name
-      JWT_SOURCE_COOKIE_REGEX = var.jwt_source_cookie_regex
+      JWT_AUDIENCE            = var.jwt_config.audience != null ? join(",", var.jwt_config.audience) : null
+      JWT_COGNITO_CLIENT_ID   = var.jwt_config.cognito_client_id != null ? join(",", var.jwt_config.cognito_client_id) : null
+      JWT_COGNITO_GROUP       = var.jwt_config.cognito_group != null ? join(",", var.jwt_config.cognito_group) : null
+      JWT_COGNITO_TOKEN_USE   = var.jwt_config.cognito_token_use
+      JWT_ISSUER              = var.jwt_config.issuer
+      JWT_SCOPE               = var.jwt_config.scope != null ? join(",", var.jwt_config.scope) : null
+      JWT_SOURCE_HEADER_NAME  = var.jwt_config.source_header_name
+      JWT_SOURCE_COOKIE_REGEX = var.jwt_config.source_cookie_regex
     }
   }
 }
--- aws/lambda/jwt-authorizer/variables.tf
+++ aws/lambda/jwt-authorizer/variables.tf
@@ -16,44 +16,21 @@ variable "cloudwatch_log_group_kms_key_id" {
   default = null
 }
 
-variable "jwt_audience" {
-  type = list(string)
-}
-
-variable "jwt_issuer" {
-  type = string
-}
-
-variable "jwt_scope" {
-  type = list(string)
-}
-
-variable "jwt_source_header_name" {
-  type    = string
-  default = "Authorization"
-}
-
-variable "jwt_source_cookie_regex" {
-  type = string
-}
-
-variable "jwt_cognito_client_id" {
-  type    = list(string)
-  default = null
-}
-
-variable "jwt_cognito_group" {
-  type    = list(string)
-  default = null
-}
-
-variable "jwt_cognito_token_use" {
-  type    = string
-  default = null
+variable "jwt_config" {
+  type = object({
+    audience            = list(string)
+    issuer              = string
+    scope               = optional(list(string), null)
+    source_header_name  = optional(string, "Authorization")
+    source_cookie_regex = optional(string, null)
+    cognito_client_id   = optional(list(string), null)
+    cognito_group       = optional(string, null)
+    cognito_token_use   = optional(string, null)
+  })
 
   validation {
-    condition     = var.jwt_cognito_token_use == "access" || var.jwt_cognito_token_use == "id" || var.jwt_cognito_token_use == null
-    error_message = "jwt_cognito_token_use value must be either 'access', 'id' or null"
+    condition     = var.jwt_config.cognito_token_use == "access" || var.jwt_config.cognito_token_use == "id" || var.jwt_config.cognito_token_use == null
+    error_message = "jwt_config.cognito_token_use value must be either 'access', 'id' or null"
   }
 }
 
