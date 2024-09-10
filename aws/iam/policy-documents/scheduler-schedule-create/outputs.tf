43d579c9f4c2c492a973004faec13b65fdb2e5e9
SebRamsland <mail@sebastianramsland.com>
2024-09-10 08:58:04 +0200

    Remove module

--- aws/iam/policy-documents/scheduler-schedule-create/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "json" {
-  value = data.aws_iam_policy_document.this.json
-}
--- aws/iam/policy-documents/scheduler-schedule-create/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "scheduler_schedule_arn" {
-  type = string
-}
-
-variable "scheduler_schedule_execution_role_arn" {
-  type = string
-}
