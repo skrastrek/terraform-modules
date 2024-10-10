26a0adfc81ede1868e01059ddf6334fabf638ed2
SebRamsland <mail@sebastianramsland.com>
2024-10-10 14:12:07 +0200

    Add debug logging

--- aws/lambda/image-processor/resources/src/index.ts
+++ aws/lambda/image-processor/resources/src/index.ts
@@ -39,7 +39,7 @@ export const handler: S3ObjectEventHandler = async event => {
         originalImageResponse = await axios.get(event.getObjectContext.inputS3Url, {responseType: 'arraybuffer'})
         console.debug(JSON.stringify(originalImageResponse))
     } catch (error) {
-        console.warn(JSON.stringify(error.toJSON()))
+        console.warn(JSON.stringify(error))
         if (error.response?.status === 403 || error.response?.status === 404) {
             await s3Client.send(
                 new WriteGetObjectResponseCommand({
